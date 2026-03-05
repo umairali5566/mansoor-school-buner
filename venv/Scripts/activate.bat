752cf0e8d46f659fb5da1bacc7e9688418c4f2c7
Umair Khan <uk8733079@gmail.com>
2026-03-05 06:02:28 +0500

    fix apscheduler

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\USER\Desktop\smart_school_system\venv
+set VIRTUAL_ENV=C:\Users\USER\Desktop\Mansoor School Buner\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
